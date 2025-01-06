30bdee825722a0e287289ba9ed4ed0b8bfe12b82
Julián Laterza <laterzajulian@gmail.com>
2025-01-06 19:20:44 -0300

    create login screen

---
+++ lib/features/auth/presentation/screens/login_screen.dart
@@ -0,0 +1,129 @@
+import 'package:flutter/material.dart';
+import 'package:juvuit_flutter/core/utils/colors.dart';
+import 'package:juvuit_flutter/core/widgets/social_login_button.dart';
+import 'package:juvuit_flutter/features/auth/presentation/screens/register_screen.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 255, 255, 255), // Fondo blanco
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 40), // Espacio superior
+              const Center(
+                child: Column(
+                  children: [
+                    Text('J U V U I T',
+                    style: TextStyle(
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold,
+                      color: AppColors.black,
+                    ),
+                    ),
+                    SizedBox(height: 10),
+                    Text('Inicia sesión para continuar',style: TextStyle(fontSize: 16,color: AppColors.gray),
+              ),
+
+                  ],
+                  ),
+              ),
+              const SizedBox(height: 40),
+              // Email Input
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Correo electrónico',
+                  labelStyle: const TextStyle(color: AppColors.darkGray),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                    borderSide: const BorderSide(color: AppColors.lightGray),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                    borderSide: const BorderSide(color: AppColors.yellow),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              // Password Input
+              TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: 'Contraseña',
+                  labelStyle: const TextStyle(color: AppColors.darkGray),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                    borderSide: const BorderSide(color: AppColors.lightGray),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                    borderSide: const BorderSide(color: AppColors.yellow),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 30),
+              // Login Button
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {
+                    // TODO: Lógica de autenticación
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: AppColors.yellow,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    padding: const EdgeInsets.symmetric(vertical: 16),
+                  ),
+                  child: const Text(
+                    'Iniciar sesión',
+                    style: TextStyle(
+                      color: AppColors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              // Forgot Password & Sign Up
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text('¿No tienes cuenta?',style: TextStyle(color: AppColors.gray)),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const RegisterScreen(),
+                        ),
+                      );
+                    },
+                    child: const Text('Regístrate',style: TextStyle(color: AppColors.yellow)),
+                  ),
+                ], // Terminan los hijos del Row
+              ),
+              const SizedBox(height: 20),
+              SocialLoginButton(icon: FontAwesomeIcons.google, text: 'Continuar con Google', onPressed: (){
+                print('Google');
+              }),
+              const SizedBox(height: 20),
+              SocialLoginButton(icon: FontAwesomeIcons.apple, text: 'Continuar con Apple', onPressed: (){
+                print('Apple');
+              }),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
