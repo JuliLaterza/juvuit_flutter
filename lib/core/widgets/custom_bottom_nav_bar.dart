5ab40d2725708799b33e844b994ed4aafc67ce6d
Julián Laterza <laterzajulian@gmail.com>
2025-01-08 02:26:24 -0300

    create bottommanbar

---
+++ lib/core/widgets/custom_bottom_nav_bar.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:juvuit_flutter/features/events/presentation/screens/events_screen.dart';
+import 'package:juvuit_flutter/features/matching/presentation/screens/matching_screen.dart';
+import 'package:juvuit_flutter/features/chats/presentation/screens/chats_screen.dart';
+import 'package:juvuit_flutter/features/profile/presentation/screens/profile_screen.dart';
+import '../utils/colors.dart';
+
+class CustomBottomNavBar extends StatelessWidget {
+  final int currentIndex;
+
+  const CustomBottomNavBar({
+    super.key,
+    required this.currentIndex,
+  });
+
+  void _onItemTapped(BuildContext context, int index) {
+    if (index == currentIndex) return; // Evita recargar la misma pantalla
+
+    Widget nextScreen;
+
+    switch (index) {
+      case 0:
+        nextScreen = const EventsScreen();
+        break;
+      case 1:
+        nextScreen = const MatchingScreen();
+        break;
+      case 2:
+        nextScreen = const ChatsScreen();
+        break;
+      case 3:
+        nextScreen = const ProfileScreen();
+        break;
+      default:
+        return;
+    }
+
+    Navigator.pushAndRemoveUntil(
+      context,
+      PageRouteBuilder(
+        pageBuilder: (context, animation, secondaryAnimation) => nextScreen,
+        transitionDuration: Duration.zero,
+        reverseTransitionDuration: Duration.zero
+      ),
+      (route) => false, // Limpia el stack de navegación
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: currentIndex,
+      onTap: (index) => _onItemTapped(context, index),
+      backgroundColor: AppColors.darkmedium,
+      selectedItemColor: Colors.yellow,
+      unselectedItemColor: Colors.white,
+      selectedIconTheme: const IconThemeData(size: 30),
+      unselectedIconTheme: const IconThemeData(size: 30),
+      showSelectedLabels: false,
+      showUnselectedLabels: false,
+      type: BottomNavigationBarType.fixed, // Muestra todos los íconos sin recortar
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.event),
+          label: 'Eventos',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.local_fire_department),
+          label: 'Matching',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.chat),
+          label: 'Chats',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.person),
+          label: 'Perfil',
+        ),
+      ],
+    );
+  }
+}
