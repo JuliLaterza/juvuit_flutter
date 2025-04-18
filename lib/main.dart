44b41572ad6de5759492c12ea13770debbdb8403
Julián Laterza <laterzajulian@gmail.com>
2025-04-18 18:31:57 -0300

    solución de navbar

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,16 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       initialRoute: AppRoutes.login, // Ruta inicial
       onGenerateRoute: AppRoutes.generateRoute,
+      theme: ThemeData(
+        useMaterial3: true,
+        scaffoldBackgroundColor: Colors.white,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.black,
+          elevation: 0,
+          scrolledUnderElevation: 0
+        ),
+      ),
     );
   }
 }
