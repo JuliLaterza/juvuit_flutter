80287a808c5d76367948eba55170f1222f5fd503
Julián Laterza <laterzajulian@gmail.com>
2025-05-25 18:46:49 -0300

    nueva logica de visualización de usuarios

--- lib/features/matching/presentation/screens/matchingprofilescreen.dart
+++ lib/features/matching/presentation/screens/matchingprofilescreen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:juvuit_flutter/core/utils/colors.dart';
@@ -6,6 +7,7 @@ import 'package:juvuit_flutter/features/profile/domain/models/user_profile.dart'
 import 'package:juvuit_flutter/features/matching/controllers/matching_profiles_controller.dart';
 import '../../widgets/profile_card.dart';
 import '../../widgets/NoMoreProfilesCard.dart';
+import '../../widgets/reencounter_profile_card.dart';
 
 class MatchingProfilesScreen extends StatefulWidget {
   final Event event;
@@ -20,6 +22,7 @@ class _MatchingProfilesScreenState extends State<MatchingProfilesScreen> {
   late final MatchingProfilesController _controller;
   List<UserProfile> _profiles = [];
   bool _isLoading = true;
+  Set<String> reencounterUserIds = {};
 
   @override
   void initState() {
@@ -36,8 +39,25 @@ class _MatchingProfilesScreenState extends State<MatchingProfilesScreen> {
     if (currentUserProfile == null) return;
 
     final profiles = await _controller.loadProfiles(widget.event);
+    final matchesSnapshot = await FirebaseFirestore.instance
+        .collection('matches')
+        .where('users', arrayContains: currentUser.uid)
+        .get();
+
+    final Set<String> seenButNotChatted = {};
+    for (final doc in matchesSnapshot.docs) {
+      final data = doc.data();
+      final users = List<String>.from(data['users']);
+      final otherUserId = users.firstWhere((uid) => uid != currentUser.uid);
+      final lastMessage = data['lastMessage'];
+      if (lastMessage == null || lastMessage.toString().trim().isEmpty) {
+        seenButNotChatted.add(otherUserId);
+      }
+    }
+
     setState(() {
       _profiles = profiles;
+      reencounterUserIds = seenButNotChatted;
       _isLoading = false;
     });
   }
@@ -66,6 +86,20 @@ class _MatchingProfilesScreenState extends State<MatchingProfilesScreen> {
 
           final profile = _profiles[index];
           final currentIndex = _controller.currentCarouselIndex[index] ?? 0;
+
+          if (reencounterUserIds.contains(profile.userId)) {
+            return ReencounterProfileCard(
+              profile: profile,
+              index: index,
+              currentImageIndex: currentIndex,
+              onDislike: () => _controller.onDislike(_profiles),
+              onCarouselChange: (imgIndex) {
+                setState(() {
+                  _controller.currentCarouselIndex[index] = imgIndex;
+                });
+              },
+            );
+          }
 
           return ProfileCard(
             profile: profile,
