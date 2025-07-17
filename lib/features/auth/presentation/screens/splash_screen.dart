acc318cd6b9612f805b338f57e5caec8331cd5b0
Julián Laterza <laterzajulian@gmail.com>
2025-07-17 00:09:12 -0300

    upd splash_screen

--- lib/features/auth/presentation/screens/splash_screen.dart
+++ lib/features/auth/presentation/screens/splash_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:juvuit_flutter/core/utils/routes.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -18,8 +19,19 @@ class _SplashScreenState extends State<SplashScreen> {
 
   Future<void> _navigate() async {
     await Future.delayed(const Duration(milliseconds: 2000));
-    final user = FirebaseAuth.instance.currentUser;
+
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.remove('onboarding_seen'); // Remove onboarding seen flag SOLO PARA DESARROLLO.
+    final onboardingSeen = prefs.getBool('onboarding_seen') ?? false;
+
     if (!mounted) return;
+
+    if (!onboardingSeen) {
+      Navigator.pushReplacementNamed(context, AppRoutes.onboarding);
+      return;
+    }
+
+    final user = FirebaseAuth.instance.currentUser;
     if (user == null) {
       Navigator.pushReplacementNamed(context, AppRoutes.login);
     } else {
@@ -30,10 +42,10 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white, 
+      backgroundColor: Colors.white,
       body: SizedBox.expand(
         child: Image.asset(
-          'assets/images/homescreen/home.png', // ajusta tamaño si querés
+          'assets/images/homescreen/home.png',
           fit: BoxFit.fill,
         ),
       ),
